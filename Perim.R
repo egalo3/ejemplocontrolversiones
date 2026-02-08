bd2a761fda79a4aa59cfc9547998693e2ef44428
egalo3 <egalo3@alumni.uv.es>
2026-02-08 14:26:51 +0100

    Cambio pi

--- Perim.R
+++ Perim.R
@@ -2,7 +2,7 @@ PerimetroCuadrado <- function (L){
   return(4*L)
 }
 Longitudcircunf <- function (radio){
-  return (2*pi*radio)
+  return (2* 3.14159*radio)
 }
 PerimetroRectangulo <- function (largo, ancho){
   return (2*(largo + ancho))
